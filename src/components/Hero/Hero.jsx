bab13b057f29ffd93ea3253719639d7a02642a96
MohianAuion <ahmohian07.gmail.com>
2026-07-19 01:43:36 +0600

    third commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
       id="home"
       className="min-h-screen bg-gradient-to-b from-white to-slate-100 flex items-center pt-24 mt-10 lg:mt-0"
     >
-      <div className="max-w-7xl mx-auto px-6 grid lg:grid-cols-2 gap-12 items-center">
+      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2 gap-12 items-center">
 
         {/* Image */}
 
